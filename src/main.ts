7daed795973fbf6df3e99210d93545b5392058ea
ayeolakenny <ayeolakenny@gmail.com>
2026-06-14 19:37:16 +0100

    add dev-portal to CORS allowed origins

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,7 @@ async function bootstrap() {
       'http://127.0.0.1:5173',
       'https://portal-mastercraft-products-com.onrender.com',
       'https://portal.mastercraft-products.com',
+      'https://dev-portal.mastercraft-products.com',
     ],
     credentials: true,
   });
